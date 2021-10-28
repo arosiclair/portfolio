f68992a7ab6f250f1c79ff5459e18218283d49fe
arosiclair <arosiclair@gmail.com>
2021-10-27 22:45:10 -0400

    added basic layout

---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import Nav from './nav'
+
+const PageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+const ContentContainer = styled.div`
+    width: max(850px, 1vw);
+    display: flex;
+`
+const MainContainer = styled.main`
+    flex: 1;
+`
+const NavConatiner = styled.nav`
+    width: 200px;
+`
+const Layout = ({ children }) => {
+    return <PageContainer>
+        <ContentContainer>
+           <MainContainer>
+                {children}
+            </MainContainer>
+            <NavConatiner>
+                <Nav />
+            </NavConatiner> 
+        </ContentContainer>
+    </PageContainer>
+}
+
+export default Layout;
---
+++ src/components/nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return <ul>
+        <li>Resume</li>
+        <li>GitHub</li>
+        <li>Email</li>
+    </ul>
+}
+
+export default Nav
