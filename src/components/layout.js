a75b3386f464ee342cfd2d6f127ea1d1573e59a3
arosiclair <arosiclair@gmail.com>
2021-10-28 23:18:40 -0400

    configured eslint and prettier

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,33 @@
-import * as React from 'react'
-import styled from 'styled-components'
-import Nav from './nav'
+import * as React from "react";
+import styled from "styled-components";
+import Nav from "./nav";
 
 const PageContainer = styled.div`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
 const ContentContainer = styled.div`
-    width: max(850px, 1vw);
-    display: flex;
-`
+  width: max(850px, 1vw);
+  display: flex;
+`;
 const MainContainer = styled.main`
-    flex: 1;
-`
+  flex: 1;
+`;
 const NavConatiner = styled.nav`
-    width: 200px;
-`
+  width: 200px;
+`;
+
 const Layout = ({ children }) => {
-    return <PageContainer>
-        <ContentContainer>
-           <MainContainer>
-                {children}
-            </MainContainer>
-            <NavConatiner>
-                <Nav />
-            </NavConatiner> 
-        </ContentContainer>
+  return (
+    <PageContainer>
+      <ContentContainer>
+        <MainContainer>{children}</MainContainer>
+        <NavConatiner>
+          <Nav />
+        </NavConatiner>
+      </ContentContainer>
     </PageContainer>
-}
+  );
+};
 
 export default Layout;
