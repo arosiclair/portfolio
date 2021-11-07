8bb7f97b5eaccc0575ca53c6e6f854d74a70a2b6
arosiclair <arosiclair@gmail.com>
2021-11-07 18:04:33 -0500

    bump down sbu icon size

--- src/components/education.js
+++ src/components/education.js
@@ -3,12 +3,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import theme from '../theme';
 import Timestamp from './timestamp';
-
-const EducationDetails = styled.p``;
-
 const EducationDetailsIcon = styled.span`
-  display: inline-block;
-  vertical-align: middle;
   margin-right: ${theme.spacing(1)};
 `;
 
@@ -16,21 +11,22 @@ const EducationSection = () => {
   return (
     <section>
       <h2>Education</h2>
+
       <Timestamp>2013 - 2017</Timestamp>
       <h3>Bachelor of Science, Computer Science</h3>
-      <EducationDetails>
+      <p>
         <EducationDetailsIcon>
           <StaticImage
             src="../images/icons/sbu.svg"
             alt=""
-            width={30}
-            height={30}
+            width={25}
+            height={25}
             objectFit="contain"
             as="span"
           />
         </EducationDetailsIcon>
         Stony Brook University
-      </EducationDetails>
+      </p>
     </section>
   );
 };
