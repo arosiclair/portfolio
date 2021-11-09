21e31642ff5c6a8b69af233e6bd63319b322394e
arosiclair <arosiclair@gmail.com>
2021-11-08 22:20:09 -0500

    remove added semicolon for spacing func

--- src/spacing.js
+++ src/spacing.js
@@ -4,9 +4,7 @@ export default (...args) => {
   if (!args.length) {
     return '0px';
   } else if (args.length <= 4) {
-    return (
-      args.map((multiplier) => `${multiplier * BASE_SIZE}px`).join(' ') + ';'
-    );
+    return args.map((multiplier) => `${multiplier * BASE_SIZE}px`).join(' ');
   } else {
     console.error('[spacing] invalid spacing args provided: ' + args);
   }
