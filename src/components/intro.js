cc0404b2eaac67e2865b8917602f7a79ca8eed17
arosiclair <arosiclair@gmail.com>
2021-11-08 21:57:11 -0500

    first person intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,10 +7,10 @@ const IntroText = styled.p`
 
 const IntroSection = () => (
   <section>
-    <h1>Welcome</h1>
+    <h1>Welcome,</h1>
     <IntroText>
-      Andrew Rosiclair is a Software Engineer with 5 years of experience
-      focusing on Full-stack development of modern web and mobile applications.
+      I&apos;m a Software Engineer with 5 years of experience focusing on
+      Full-stack development of modern web and mobile applications.
     </IntroText>
   </section>
 );
