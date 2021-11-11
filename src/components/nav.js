ff1d201ea6bb3467edecd9dd1ddf48bc4106626c
arosiclair <arosiclair@gmail.com>
2021-11-10 21:54:23 -0500

    nudge over nav sidebar

--- src/components/nav.js
+++ src/components/nav.js
@@ -11,7 +11,7 @@ const NavContainer = styled.nav`
   justify-content: space-around;
 
   @media ${theme.mediaQueries.tablet} {
-    padding: ${theme.spacing(6, 2)};
+    padding: ${theme.spacing(6, 4)};
     flex-direction: column;
     justify-content: normal;
     overflow: hidden;
