361e4be753747d1bac496ee008c7c7161bffb815
arosiclair <arosiclair@gmail.com>
2021-11-13 19:11:03 -0500

    fix dynamic years of experience to run correctly after hydration

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,25 +1,29 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const IntroText = styled.p`
   font-size: 1.2em;
 `;
 
-const getYearsOfExperience = () => {
-  const msSinceBegin = new Date() - new Date(2017, 4, 0);
-  const msInYear = 31536000730;
-  return (msSinceBegin / msInYear).toFixed(1);
-};
+const IntroSection = () => {
+  const [yearsOfExperience, setYearsOfExperience] = useState('...');
+  useEffect(() => {
+    const msSinceBegin = new Date() - new Date(2017, 4, 0);
+    const msInYear = 31536000730;
+    const result = (msSinceBegin / msInYear).toFixed(1);
+    setYearsOfExperience(result);
+  }, []);
 
-const IntroSection = () => (
-  <section>
-    <h1>Welcome,</h1>
-    <IntroText>
-      I&apos;m a Software Engineer with {getYearsOfExperience()} years of
-      experience focusing on Full-stack development of modern web and mobile
-      applications.
-    </IntroText>
-  </section>
-);
+  return (
+    <section>
+      <h1>Welcome,</h1>
+      <IntroText>
+        I&apos;m a Software Engineer with {yearsOfExperience} years of
+        experience focusing on Full-stack development of modern web and mobile
+        applications.
+      </IntroText>
+    </section>
+  );
+};
 
 export default IntroSection;
