fb6329ec11a1a771b2d4768bdc2834a89f57d673
arosiclair <arosiclair@gmail.com>
2021-11-15 23:22:30 -0500

    added page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import SkillsSection from '../components/skills';
 import ExperienceSection from '../components/experience';
 import ProjectsSection from '../components/projects';
 import EducationSection from '../components/education';
+import { Helmet } from 'react-helmet';
 
 // markup
 const IndexPage = () => {
   return (
     <Layout>
+      <Helmet>
+        <title>Andrew Rosiclair&apos;s Portfolio</title>
+      </Helmet>
       <IntroSection />
       <SkillsSection />
       <EducationSection />
